1014d83ef16733286075acb0f1b73b7570eeb694
Mike Bostock <michael.bostock@opencastsoftware.com>
2021-12-01 16:42:34 +0000

    add tests for part two

--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -20,4 +20,28 @@ internal class Day01Test
         val depths = listOf(1, 2, 3, 4, 3, 2, 1)
         assertEquals(3, Day01.partOne(depths))
     }
+
+    @Test
+    internal fun noIncrementsIfOnlyOneTriplet() {
+        val depths = listOf(3, 2, 1)
+        assertEquals(0, Day01.partTwo(depths))
+    }
+
+    @Test
+    internal fun noIncrementsIfAllTripletsDecrement() {
+        val depths = listOf(9, 8, 7, 6, 5, 4, 3, 2, 1)
+        assertEquals(0, Day01.partTwo(depths))
+    }
+
+    @Test
+    internal fun oneLessThanNumOfTripletsIfAllTripletsIncrement() {
+        val depths = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+        assertEquals(6, Day01.partTwo(depths))
+    }
+
+    @Test
+    internal fun correctIncrementsIfMixedTripletsIncrementAndDecrement() {
+        val depths = listOf(1, 2, 3, 1, 1, 6, 7, 8, 9)
+        assertEquals(4, Day01.partTwo(depths))
+    }
 }
